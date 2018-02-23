7519f27a9e10d53bf8826dca2c3beace11f24dca
WJPei <b06902029@csie.ntu.edu.tw>
2018-02-23 17:16:05 +0800

    Put events on rails server successfully
    Change bootstrap to v4.0.0.
    Modify .js, .scss

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,49 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require_tree .
+//= require ckeditor/init
+//= require turbolinks
+//= require jquery
+//= require jquery3
+//= require popper
+//= require bootstrap
+
+// start of event.js
+
+$(function(){
+	var url = window.location.href;
+	var activeTab = url.substring(url.indexOf("#") + 1);
+	$(".tab-pane").removeClass("active in");
+	$("#" + activeTab).addClass("active in");
+	$('a[href="#'+ activeTab +'"]').tab('show');
+});
+
+$(function(){
+	var hash = window.location.hash;
+	hash && $('ul.nav a[href="' + hash + '"]').tab('show');
+
+	$('.nav-tabs a').click(function (e) {
+		$(this).tab('show');
+		var scrollmem = $('body').scrollTop() || $('html').scrollTop();
+		window.location.hash = this.hash;
+		$('html,body').scrollTop(scrollmem);
+	});
+});
+
+// end of event.js// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
 //= require turbolinks
 //= require_tree .
 //= require ckeditor/init
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ class PagesController < ApplicationController
   end
 
   def event
-    @events = Event.all
+    @music_events = Event.where("category"=>"歌")
+    @dance_events = Event.where("category"=>"舞")
+    @light_events = Event.where("category"=>"光")
+    @drama_events = Event.where("category"=>"劇")
   end
 end
