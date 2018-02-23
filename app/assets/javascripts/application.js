08f7acad3e8e22ccc5cf6b48e8142b7ba880b282
Po-wen Chen <jacky87810@gmail.com>
2018-02-23 17:58:41 +0800

    Add partial to page#event

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,11 +16,11 @@
 //= require turbolinks
 //= require jquery
 //= require jquery3
-//= require popper
+// = require popper
 //= require bootstrap
 
-// start of event.js
 
+// start of event.js
 $(function(){
 	var url = window.location.href;
 	var activeTab = url.substring(url.indexOf("#") + 1);
@@ -40,20 +40,4 @@ $(function(){
 		$('html,body').scrollTop(scrollmem);
 	});
 });
-
-// end of event.js// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require rails-ujs
-//= require turbolinks
-//= require_tree .
-//= require ckeditor/init
+// end of event.js
