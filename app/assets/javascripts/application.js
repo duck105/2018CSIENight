216a6ef5b1e3a61478ac4407bbbf67f88ce461d9
Po-wen Chen <jacky87810@gmail.com>
2018-02-26 13:14:56 +0800

    Nodify js load order

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery3
 //= require rails-ujs
 //= require_tree .
 //= require ckeditor/init
 //= require turbolinks
-//= require jquery
-//= require jquery3
 //= require popper
 //= require bootstrap
 
