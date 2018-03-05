b5e3c9011a93620f0cdf5d1c6b62af0f4d73e3d2
Po-wen Chen <jacky87810@gmail.com>
2018-03-05 18:02:50 +0800

    Add db index and remove devise registration button

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :trackable, :validatable, :registerable, :rememberable
+  devise :database_authenticatable, :trackable, :validatable, :rememberable
 
   mount_uploader :avatar, AvatarUploader
   belongs_to :department,  optional: true
