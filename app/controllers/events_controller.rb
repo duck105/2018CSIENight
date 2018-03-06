ecf6fbe54561337222f819575b5d61aea3b19250
Po-wen Chen <jacky87810@gmail.com>
2018-03-06 12:46:39 +0800

    Fix event ordering

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   def index
     @music_events = Event.where("category"=>"歌").order("order" => "asc")
     @dance_events = Event.where("category"=>"舞").order("order" => "asc")
-    @light_events = Event.where("category"=>"光").order("order" => "asc")
     @drama_events = Event.where("category"=>"劇").order("order" => "asc")
+    @light_events = Event.where("category"=>"光").order("order" => "asc")
   end
 end
